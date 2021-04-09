1d36c8e274cf7f10b547e2cca321e72d77cc3643
Nurverek <nurverek@gmail.com>
2021-04-09 19:13:11 +0430

    Added option to disable compensator, also changed extrananos type to int

--- src/main/java/vanguard/VLVThreadTask.java
+++ src/main/java/vanguard/VLVThreadTask.java
@@ -5,21 +5,33 @@ public class VLVThreadTask implements VLThreadTaskType<VLThreadWorker>{
     public final VLVTypeRunner root;
 
     private final long freqmillis;
-    private final long freqextrananos;
+    private final int freqextrananos;
+
     private final boolean debug;
+    private final boolean enablecompensator;
 
     private final PostReporter reporter;
 
-    public VLVThreadTask(VLVTypeRunner root, long freqmillis, long freqextrananos, boolean debug, PostReporter reporter){
+    public VLVThreadTask(VLVTypeRunner root, long freqmillis, int freqextrananos, boolean debug, boolean enablecompensator, PostReporter reporter){
         this.root = root;
         this.freqmillis = freqmillis;
         this.freqextrananos = freqextrananos;
         this.debug = debug;
+        this.enablecompensator = enablecompensator;
         this.reporter = reporter;
     }
 
     @Override
     public void run(VLThreadWorker worker){
+        if(enablecompensator){
+            runWithCompensator(worker);
+
+        }else{
+            runDirect(worker);
+        }
+    }
+
+    private void runWithCompensator(VLThreadWorker worker){
         long offsettime = 0;
         long elapsednanos = 0;
         long frequencynanos = freqmillis * 1000000 + freqextrananos;
@@ -69,6 +81,36 @@ public class VLVThreadTask implements VLThreadTaskType<VLThreadWorker>{
                 VLDebug.append(elapsednanos - frequencynanos);
                 VLDebug.append("ns]");
                 VLDebug.printD();
+            }
+        }
+    }
+
+    private void runDirect(VLThreadWorker worker){
+        int changes = 0;
+
+        while(worker.isEnabled()){
+            synchronized(root){
+                changes = root.next();
+            }
+
+            reporter.completed(changes);
+
+            if(changes == 0){
+                try{
+                    synchronized(root){
+                        root.wait();
+                    }
+
+                }catch(InterruptedException ex){
+                    //
+                }
+            }
+
+            try{
+                Thread.sleep(freqmillis, freqextrananos);
+
+            }catch(InterruptedException ex){
+                ex.printStackTrace();
             }
         }
     }

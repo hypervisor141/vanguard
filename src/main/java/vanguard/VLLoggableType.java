ecf4d07340799c946011cbeae8812f25484c6384
Nurverek <nurverek@gmail.com>
2021-05-15 15:58:19 +0430

    Added a helper class for direct printing

--- src/main/java/vanguard/VLLoggableType.java
+++ src/main/java/vanguard/VLLoggableType.java
@@ -6,8 +6,8 @@ public interface VLLoggableType{
 
     final class Helper{
 
-        public String getString(VLLoggableType target, Object data){
-            VLLog log = new VLLog(0);
+        public String getString(String tag, VLLoggableType target, Object data){
+            VLLog log = new VLLog(tag, 1);
             target.log(log, data);
 
             return log.get().toString();
